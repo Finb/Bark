6269c85080bfed8393aef5cc870d4fdcaa98f1b4
Fin <heyfiniks@gmail.com>
2018-03-07 16:43:47 +0800

    Initial commit

---
+++ Bark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bark
+//
+//  Created by huangfeng on 2018/3/7.
+//  Copyright © 2018年 Fin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+platform:ios,'9.0'
+inhibit_all_warnings!
+use_frameworks!
+
+def pods
+    pod 'KVOController'
+    pod 'SVProgressHUD'
+    pod 'MJRefresh'
+    pod 'FDFullscreenPopGesture'
+    pod 'Moya/RxSwift'
+    pod 'DeviceKit'
+end
+
+target 'Bark' do
+    pods
+end
