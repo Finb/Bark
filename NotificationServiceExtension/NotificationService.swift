4ae5086ef05b3ccd7ba77fa60598ebd326d62b1f
Fin <heyfiniks@gmail.com>
2018-12-17 17:37:06 +0800

    添加『收到推送时，自动复制推送内容』的参数

---
+++ NotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationService.swift
+//  NotificationServiceExtension
+//
+//  Created by huangfeng on 2018/12/17.
+//  Copyright © 2018 Fin. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+    
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+    
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            let userInfo = bestAttemptContent.userInfo
+            if userInfo["AutomaticallyCopy"] as? Bool == true{
+                if let copy = userInfo["copy"] as? String {
+                    UIPasteboard.general.string = copy
+                }
+                else{
+                    UIPasteboard.general.string = bestAttemptContent.body
+                }
+            }
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+}
