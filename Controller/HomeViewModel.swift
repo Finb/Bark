abe4c4e4198c289425bef5063cd56ae11ab91b6b
Fin <heyfiniks@gmail.com>
2020-11-18 16:05:21 +0800

    调整文件目录

---
+++ Controller/HomeViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewModel.swift
+//  Bark
+//
+//  Created by huangfeng on 2020/11/18.
+//  Copyright © 2020 Fin. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class HomeViewModel: ViewModel, ViewModelType {
+    struct Input {
+        
+    }
+    struct Output {
+        
+    }    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+    
+}
