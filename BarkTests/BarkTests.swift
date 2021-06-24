f7c34f627760b735edf2ae8db43515b68e04a8a9
Fin <heyfiniks@gmail.com>
2021-06-24 12:35:30 +0800

    添加单元测试

---
+++ BarkTests/BarkTests.swift
@@ -0,0 +1,33 @@
+//
+//  BarkTests.swift
+//  BarkTests
+//
+//  Created by huangfeng on 2021/6/24.
+//  Copyright © 2021 Fin. All rights reserved.
+//
+
+import XCTest
+
+class BarkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
